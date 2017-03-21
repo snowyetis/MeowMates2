21c3ec63ed7a581b21de4988370e540034ecfc29
Joe <josephd.carlson@gmail.com>
2017-03-21 04:16:15 -0400

    more fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     <%= ENV['aws_access_key_id'] %>,
-      aws_secret_access_key: <%= ENV['aws_secret_access_key'] %>,
+      aws_access_key_id:     Rails.application.secrets.aws_access_key_id, # required
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
       region:                'us-east-1',
     }
     config.fog_directory  = 'meowmates'
