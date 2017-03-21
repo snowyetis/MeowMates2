9059f7d542ebc468c4d375ec3c6e6b74efae10fa
Joe <josephd.carlson@gmail.com>
2017-03-21 05:26:11 -0400

    BKA

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,9 +6,15 @@ CarrierWave.configure do |config|
       aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
       region:                'us-east-1',
     }
-    config.fog_directory  = 'meowmates'
+
+    config.fog_directory  = connection.directories.create(
+      :key    => "meowmates-#{Time.now.to_i}", # globally unique name
+      :public => true
+    )
+
   end
 end
 
 # config.fog_public     = true
+# config.fog_directory  = 'meowmates'
 # config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
