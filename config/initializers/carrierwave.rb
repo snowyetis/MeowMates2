090c9eceecfd6d9769d3822d6830a8925d42c29e
Joe <josephd.carlson@gmail.com>
2017-03-21 04:40:56 -0400

    f

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,5 @@
 CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'                        # required
   if Rails.env.production?
     config.fog_credentials = {
       provider:              'AWS',
@@ -6,7 +7,7 @@ CarrierWave.configure do |config|
       aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
       region:                'us-east-1',
     }
-    config.fog_directory  = 'meowmates'
+    config.fog_directory  = 'avatar_uploads'
     config.fog_public     = true
     config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
   end
