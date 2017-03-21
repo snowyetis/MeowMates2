78e831e6881564d143890c8f6aa1d1de8b1c326b
Joe <josephd.carlson@gmail.com>
2017-03-21 04:04:40 -0400

    Configula

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,13 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
     config.fog_credentials = {
-      provider:              'AWS', # required
-      aws_access_key_id:     Rails.application.secrets.aws_access_key_id, # required
-      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
-      region:                'us-east-1', # optional, defaults to 'us-east-1'
+      provider:              'AWS',
+      aws_access_key_id:     <%= ENV['aws_access_key_id'] %>,
+      aws_secret_access_key: <%= ENV['aws_secret_access_key'] %>,
+      region:                'us-east-1',
     }
-    config.fog_directory  = 'meowmates' # required
-    config.fog_public     = true # optional, defaults to true
-    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+    config.fog_directory  = 'meowmates'
+    config.fog_public     = true
+    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
   end
 end
