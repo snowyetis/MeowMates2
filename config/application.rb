882bbcc698c0840873296ca2bd10ca46ab267d62
Joe <josephd.carlson@gmail.com>
2017-06-13 21:03:45 -0400

    more rescue...

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'fog/aws'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Socify
+module MeowMates
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -20,5 +20,6 @@ module Socify
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.active_job.queue_adapter = :resque
   end
 end
